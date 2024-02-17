21e3416ae799755c55b5ab8c1ee2793531ce2e73
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2024-02-17 01:27:54 -0300

    implementing method of sending email

--- src/main/java/com/micro/service/controlllers/EmailController.java
+++ src/main/java/com/micro/service/controlllers/EmailController.java
@@ -1,14 +1,30 @@
 package com.micro.service.controlllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.micro.service.dtos.EmailDto;
+import com.micro.service.models.Email;
 import com.micro.service.services.EmailService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class EmailController {
 	
 	@Autowired
 	EmailService emailService;
+	
+	public ResponseEntity<Email> sendingEmail (@RequestBody @Valid EmailDto emailDto){
+		Email email = new Email();
+		BeanUtils.copyProperties(emailDto, email);
+		emailService.sendEmail(email);
+		return new ResponseEntity<>(email, HttpStatus.CREATED);
+		
+	}
 
 }
--- src/main/java/com/micro/service/services/EmailService.java
+++ src/main/java/com/micro/service/services/EmailService.java
@@ -3,12 +3,22 @@ package com.micro.service.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.micro.service.dtos.EmailDto;
+import com.micro.service.models.Email;
 import com.micro.service.repositories.EmailRepository;
+
+import jakarta.validation.Valid;
 
 @Service
 public class EmailService {
 	
 	@Autowired
 	EmailRepository emailRepository;
+
+	public void sendEmail(Email email) {
+		emailRepository.save(email);
+		
+		
+	}
 
 }
