683f982be0fd00e42971d8fe98466608e5c19293
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2024-02-18 18:35:52 -0300

    removing unusable code

--- src/main/java/com/micro/service/services/EmailService.java
+++ src/main/java/com/micro/service/services/EmailService.java
@@ -1,28 +1,45 @@
 package com.micro.service.services;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import com.micro.service.dtos.EmailDto;
+import com.micro.service.enums.StatusEmail;
 import com.micro.service.models.Email;
 import com.micro.service.repositories.EmailRepository;
 
-import jakarta.validation.Valid;
+import jakarta.transaction.Transactional;
 
 @Service
 public class EmailService {
 	
-	@Autowired
-	EmailRepository emailRepository;
+    @Autowired
+	 EmailRepository emailRepository;
 	
-	@Autowired
-	JavaMailSender emailSender;
+    @Autowired
+	 JavaMailSender emailSender;
 
-	public void sendEmail(Email email) {
-		emailRepository.save(email);
-		
-		
-	}
+    @Transactional
+    public Email sendEmail(Email emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return emailRepository.save(emailModel);
+        }
+    }
 
 }
