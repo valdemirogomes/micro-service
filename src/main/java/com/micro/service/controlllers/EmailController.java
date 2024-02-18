d257e42ff0bf875b1cc48cb6178ae810c593ac6f
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2024-02-18 18:33:50 -0300

    implementing the @RestController annotation

--- src/main/java/com/micro/service/controlllers/EmailController.java
+++ src/main/java/com/micro/service/controlllers/EmailController.java
@@ -4,8 +4,9 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.micro.service.dtos.EmailDto;
 import com.micro.service.models.Email;
@@ -13,12 +14,13 @@ import com.micro.service.services.EmailService;
 
 import jakarta.validation.Valid;
 
-@Controller
+@RestController
 public class EmailController {
 	
-	@Autowired
-	EmailService emailService;
-	
+    @Autowired
+	 EmailService emailService;
+    
+	@PostMapping("/sending-email")
 	public ResponseEntity<Email> sendingEmail (@RequestBody @Valid EmailDto emailDto){
 		Email email = new Email();
 		BeanUtils.copyProperties(emailDto, email);
