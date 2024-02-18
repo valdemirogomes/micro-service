5ae78835ef352c963099d7f8e611388c71d96393
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2024-02-18 18:37:17 -0300

    implementing @Repository annotation

--- src/main/java/com/micro/service/repositories/EmailRepository.java
+++ src/main/java/com/micro/service/repositories/EmailRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import com.micro.service.models.Email;
 
-@Repository
+
 public interface EmailRepository extends JpaRepository<Email, UUID>{
 
 }
